73ff11cfb487c52a35d353d9a41d00302cd1aaf6
Danny Demosi <ddemosi@remodal.io>
2020-09-29 13:01:37 -0600

    pages commit

---
+++ precache-manifest.0383be3c67116bddd56fa208c4b8c97b.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "860c607871d45b805f702c2d3bfb066f",
+    "url": "/around-react/index.html"
+  },
+  {
+    "revision": "7b155cd9eb2fd381bc71",
+    "url": "/around-react/static/css/main.9a5ce7ac.chunk.css"
+  },
+  {
+    "revision": "1a78164a4985df51e50d",
+    "url": "/around-react/static/js/2.bb94de64.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/around-react/static/js/2.bb94de64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b155cd9eb2fd381bc71",
+    "url": "/around-react/static/js/main.ca4842fe.chunk.js"
+  },
+  {
+    "revision": "71330f07cffbc9893e39",
+    "url": "/around-react/static/js/runtime-main.f73c9f35.js"
+  },
+  {
+    "revision": "5df32a06fdf69f3ae19600017c36d45a",
+    "url": "/around-react/static/media/Close-Icon.5df32a06.svg"
+  },
+  {
+    "revision": "b05083e51f8e21ef50c2b78b29bb740c",
+    "url": "/around-react/static/media/Edit-Button.b05083e5.svg"
+  },
+  {
+    "revision": "09f4068bb74fc78ffbce74f5596d6aa5",
+    "url": "/around-react/static/media/Inter-Black.09f4068b.woff2"
+  },
+  {
+    "revision": "e37354838bf8078ac0d296217613533f",
+    "url": "/around-react/static/media/Inter-Black.e3735483.woff"
+  },
+  {
+    "revision": "79260e5b693fac2ee373b659f01a2bdf",
+    "url": "/around-react/static/media/Inter-Bold.79260e5b.woff"
+  },
+  {
+    "revision": "aed27700d84e327fda56b4a427b03061",
+    "url": "/around-react/static/media/Inter-Bold.aed27700.woff2"
+  },
+  {
+    "revision": "4dd66a113d54a7f9a1ae913049610617",
+    "url": "/around-react/static/media/Inter-Regular.4dd66a11.woff2"
+  },
+  {
+    "revision": "7c539936c4c8c822b59a1bcc6c08a6ec",
+    "url": "/around-react/static/media/Inter-Regular.7c539936.woff"
+  },
+  {
+    "revision": "33286c6dc65271f0de96dbd3742f11a6",
+    "url": "/around-react/static/media/add-button-large.33286c6d.svg"
+  },
+  {
+    "revision": "9e35f85831d68caadf15de3f2c7450fd",
+    "url": "/around-react/static/media/add-button-small.9e35f858.svg"
+  },
+  {
+    "revision": "d476a8b86d0b656cfa463c1148e96069",
+    "url": "/around-react/static/media/edit-pencil.d476a8b8.svg"
+  },
+  {
+    "revision": "5bbb05f725aa334a73acf3174e255f4e",
+    "url": "/around-react/static/media/heart-colored.5bbb05f7.svg"
+  },
+  {
+    "revision": "83a46f45b0ce22c9a4433c11d0372f26",
+    "url": "/around-react/static/media/heart.83a46f45.svg"
+  },
+  {
+    "revision": "8f7611ae39c682f37516feec261f77dd",
+    "url": "/around-react/static/media/logo.8f7611ae.svg"
+  },
+  {
+    "revision": "d80783439d382b46ce24b8da369767d4",
+    "url": "/around-react/static/media/trash-can.d8078343.svg"
+  }
+]);
